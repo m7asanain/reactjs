2540e38ac545847f7ee52af319bff781e9fed81e
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-29 13:00:23 +0300

    specifying children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World!</h1>
+    return  (
+      // React.Fragment like a div but does'nt show like a div
+      <React.Fragment>
+        <h1>Hello World!</h1>
+        <button>Incrument</button>
+      </React.Fragment>
+    )  
   }
 }
 

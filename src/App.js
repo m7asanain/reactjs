d0222e091827eaf135167e5f739a61337a1509ca
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-29 12:25:40 +0300

    apply bootstrap

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+// 'React' must be in scope when using JSX  react/react-in-jsx-scope
+import React, { Component }  from 'react';  // to solve the above error
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

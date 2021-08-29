5b52c01b6d44238ff97208986e8b6038ce359532
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-29 15:04:49 +0300

    rendering classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,36 +3,29 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-    // imgUrl: 'https://picsum.photos/400' // to generator an image
   };
 
-  // styles = {
-  //   fontSize: 10,
-  //   fontWeight: 'bold'
-  // };
-
   render() {
+    // let classes = 'badge m-2 badge-';
+    // classes += this.state.count === 0 ? 'warning' : 'primary';
+
     return  (
-      // React.Fragment like a div but does'nt show like a div
       <React.Fragment>
-        {/* <img src={this.state.imgUrl} alt="" /> */}
-        <span className="badge badge-primary m-2">{this.formatCounter()}</span>
-        <button className="btn btn-secondary btn-sm">Incrument</button>
-
-        {/* <button style={ this.styles }>Incrument</button> */}
-        {/* inline css */}
-        {/* <button style={{ fontSize: 50 }}>Incrument</button> */} 
-
-
+        <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
+        <button className='btn btn-sm btn-secondary'>Incrument</button>
       </React.Fragment>
     )  
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCounter() {
     const { count } = this.state;
     return count === 0 ? 'Zero' : count;
-
-    // return this.state.count === 0 ? 'zero' : this.state.count;
   }
 }
 

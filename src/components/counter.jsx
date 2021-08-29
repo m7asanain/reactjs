0e0c45e85b711d4042c5efd4fa7eb4e50149b7ee
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-29 13:17:59 +0300

    embedding experssions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return  (
       // React.Fragment like a div but does'nt show like a div
       <React.Fragment>
-        <h1>Hello World!</h1>
+        <h1>{this.formatCounter()}</h1>
         <button>Incrument</button>
       </React.Fragment>
     )  
+  }
+
+  formatCounter() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+
+    // return this.state.count === 0 ? 'zero' : this.state.count;
   }
 }
 

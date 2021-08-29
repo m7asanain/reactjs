55b90e4d345dfe6898ad2fa96c0bcfa8ae5dad90
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-29 14:41:40 +0300

    setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    // imgUrl: 'https://picsum.photos/400' // to generator an image
   };
+
+  // styles = {
+  //   fontSize: 10,
+  //   fontWeight: 'bold'
+  // };
 
   render() {
     return  (
       // React.Fragment like a div but does'nt show like a div
       <React.Fragment>
-        <h1>{this.formatCounter()}</h1>
-        <button>Incrument</button>
+        {/* <img src={this.state.imgUrl} alt="" /> */}
+        <span className="badge badge-primary m-2">{this.formatCounter()}</span>
+        <button className="btn btn-secondary btn-sm">Incrument</button>
+
+        {/* <button style={ this.styles }>Incrument</button> */}
+        {/* inline css */}
+        {/* <button style={{ fontSize: 50 }}>Incrument</button> */} 
+
+
       </React.Fragment>
     )  
   }

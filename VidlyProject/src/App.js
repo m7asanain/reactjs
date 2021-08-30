7021d049b0ab88c4217b7d2eb300e177b4ca66ce
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-30 20:23:17 +0300

    Vidly Project

---
+++ VidlyProject/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{}</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
+}
+
+export default App;
